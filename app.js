38a08f2072bdb184f020370e79abfc4e68d9a32f
AFINIKI MHYA <johnafinikimhya12@gmail.com>
2024-07-05 07:47:00 +0100

    mobile view check

--- app.js
+++ app.js
@@ -1,32 +1,65 @@
-const cards = document.querySelector(".cards");
-const innerSlider = document.querySelectorAll(".coll-card");
+const cardSlider = document.querySelector(".cards-container");
+const innerCards = document.querySelector(".cards");
 
 let pressed = false;
 let startx;
 let x;
-let scrollLeft;
 
-cards.addEventListener('mousedown', (e) => {
-    isDown = true;
-    cards.classList.add('dragging');
-    startx = e.pageX - cards.offsetLeft;
-    scrollLeft = cards.scrollLeft;
+cardSlider.addEventListener("mousedown", (e) => {
+  pressed = true;
+  startx = e.offsetX - innerCards.offsetLeft;
+  cardSlider.style.cursor = "grabbing";
+  cardSlider.style.userSelect = "none";  // Prevent text selection
+  console.log(innerCards);
 });
 
-cards.addEventListener('mouseleave', () => {
-    isDown = false;
-    cards.classList.remove('dragging');
+cardSlider.addEventListener("mouseenter", () => {
+  if (!pressed) cardSlider.style.cursor = "grab";
 });
 
-cards.addEventListener('mouseup', () => {
-    isDown = false;
-    cards.classList.remove('dragging');
+cardSlider.addEventListener("mouseleave", () => {
+  cardSlider.style.cursor = "grab";
+  if (!pressed) cardSlider.style.userSelect = "";  // Restore text selection
 });
 
-cards.addEventListener('mousemove', (e) => {
-    if (!isDown) return;  // stop the fn from running
-    e.preventDefault();
-    const x = e.pageX - cards.offsetLeft;
-    const walk = (x - startx) * 3; //scroll-fast
-    cards.scrollLeft = scrollLeft - walk;
+cardSlider.addEventListener("mouseup", () => {
+  pressed = false;
+  cardSlider.style.cursor = "grab";
+  cardSlider.style.userSelect = "";  // Restore text selection
+});
+
+window.addEventListener("mouseup", () => {
+  pressed = false;
+  cardSlider.style.cursor = "grab";
+  cardSlider.style.userSelect = "";  // Restore text selection
+});
+
+cardSlider.addEventListener("mousemove", (e) => {
+  if (!pressed) return;
+  e.preventDefault();
+
+  x = e.offsetX;
+  innerCards.style.left = `${x - startx}px`;
+});
+
+
+cardSlider.addEventListener("touchstart", (e) => {
+  pressed = true;
+  startx = e.touches[0].clientX - innerCards.offsetLeft;
+  cardSlider.style.cursor = "grabbing";
+  cardSlider.style.userSelect = "none";  
+});
+
+cardSlider.addEventListener("touchend", () => {
+  pressed = false;
+  cardSlider.style.cursor = "grab";
+  cardSlider.style.userSelect = "";  
+});
+
+cardSlider.addEventListener("touchmove", (e) => {
+  if (!pressed) return;
+  e.preventDefault();
+
+  x = e.touches[0].clientX;
+  innerCards.style.left = `${x - startx}px`;
 });
